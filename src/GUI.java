becf3228d3eb8a6c46009c1d708ab453d6254659
kr3s0 <portable876@gmail.com>
2019-01-15 21:44:46 +0100

    15012144

--- src/GUI.java
+++ src/GUI.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import javax.swing.*;
 
@@ -13,71 +15,69 @@ public class GUI {
 	private JButton[][] field;
 	private JPanel panelwindow;
 	private JPanel panelMenu;
+	private JPanel panelMenuNorth;
+	private JPanel panelMenuCenter;
+	private JPanel panelMenuSouth;
+	private JButton buttonNewGame;
+	private JButton buttonEndGame;
+	private JButton buttonChangeDifficulty;
+	private JLabel showLevel;
+	private JLabel showDifficulty;
+	private JLabel showScore;
 	private JPanel panelField;
 	private static final Color SEA = new Color(0,105,148);
 	private static final Color SHIP = new Color(139,69,19);
 	private static final Color PIRATES = Color.BLACK;
 	private static final Color BARRIERS = new Color(255,228,181);
 	
-	public class EventHandler implements KeyListener {
-		@Override
-		public void keyPressed(KeyEvent e) {
-			GUI.this.enableFocus(GUI.this.g.p.getPosition(), false);
-		switch(e.getKeyCode()) {
-		case KeyEvent.VK_NUMPAD1:
-				GUI.this.g.p.moveDownLeft();
-				break;
-			case KeyEvent.VK_NUMPAD2:
-				GUI.this.g.p.moveDown();
-				break;
-			case KeyEvent.VK_NUMPAD3:
-				GUI.this.g.p.moveDownRight();
-				break;
-			case KeyEvent.VK_NUMPAD4:
-				GUI.this.g.p.moveLeft();
-				break;
-			case KeyEvent.VK_NUMPAD6:
-				GUI.this.g.p.moveRight();
-				break;
-			case KeyEvent.VK_NUMPAD7:
-				GUI.this.g.p.moveUpLeft();
-				break;
-			case KeyEvent.VK_NUMPAD8:
-				GUI.this.g.p.moveUp();
-				break;
-			case KeyEvent.VK_NUMPAD9:
-				GUI.this.g.p.moveUpRight();
-				break;
-			}
-			GUI.this.enableFocus(GUI.this.g.p.getPosition(), true);
-			if(GUI.this.g.level==1) {
-				GUI.this.g.list_Pirates.forEach((g)->g.moveToPlayer(GUI.this.g.p.getPosition(),1));
-			}
-			else {
-				GUI.this.g.list_Pirates.forEach((g)->g.moveToPlayer(GUI.this.g.p.getPosition()));
-			}
-		}
-		@Override
-		public void keyReleased(KeyEvent e) {	
-		}
-		@Override
-		public void keyTyped(KeyEvent e) {
-		}
-	}
-	
 	GUI(int r,int c){
 		this.window = new JFrame("Pirate Hunt");
 		this.panelwindow = new JPanel(new BorderLayout());
-		this.panelMenu = new JPanel();
+		this.panelMenu = new JPanel(new BorderLayout());
+		this.panelMenuNorth = new JPanel();
+		this.panelMenuCenter = new JPanel();
+		this.panelMenuSouth = new JPanel();
 		this.panelField = new JPanel(new GridLayout(r,c));
 		this.window.setContentPane(this.panelwindow);
 		this.panelMenu.setPreferredSize(new Dimension(200,700));
 		this.panelMenu.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		this.panelwindow.add(this.panelMenu,BorderLayout.LINE_START);
 		this.panelwindow.add(this.panelField,BorderLayout.CENTER);
+		this.showDifficulty = new JLabel();
+		this.showLevel = new JLabel();
+		this.showScore = new JLabel();
+		this.showDifficulty.setPreferredSize(new Dimension(100,40));
+		this.showLevel.setPreferredSize(new Dimension(100,40));
+		this.showScore.setPreferredSize(new Dimension(100,40));
+		this.showDifficulty.setBorder(BorderFactory.createTitledBorder("Difficulty"));
+		this.showLevel.setBorder(BorderFactory.createTitledBorder("Level"));
+		this.showScore.setBorder(BorderFactory.createTitledBorder("Score"));
+		this.panelMenuNorth.add(this.showScore);
+		this.panelMenuNorth.add(this.showLevel);
+		this.panelMenuNorth.add(this.showDifficulty);
+		this.panelMenuNorth.setPreferredSize(new Dimension(200,150));
+		this.buttonChangeDifficulty = new JButton("Change Difficulty");
+		this.buttonEndGame = new JButton("End Game");
+		this.buttonNewGame = new JButton("New Game");
+		this.buttonChangeDifficulty.setPreferredSize(new Dimension(150,40));
+		this.buttonEndGame.setPreferredSize(new Dimension(150,40));
+		this.buttonNewGame.setPreferredSize(new Dimension(150,40));
+		this.panelMenuCenter.add(this.buttonNewGame);
+		this.panelMenuCenter.add(this.buttonChangeDifficulty);
+		this.panelMenuCenter.add(this.buttonEndGame);
+		this.panelMenuCenter.setPreferredSize(new Dimension(200,400));
+		this.panelMenuCenter.setBackground(this.SHIP);
+		this.panelMenuNorth.setBackground(this.BARRIERS);
+		this.panelMenuSouth.setBackground(this.BARRIERS);
+		this.panelMenuSouth.setPreferredSize(new Dimension(200,150));
+		this.panelMenu.add(this.panelMenuNorth,BorderLayout.NORTH);
+		this.panelMenu.add(this.panelMenuCenter,BorderLayout.CENTER);
+		this.panelMenu.add(this.panelMenuSouth,BorderLayout.SOUTH);
 		this.g = new Game();
 		this.g.setGame(r,c);
-//		this.g.setLevel(2);
+		this.showDifficulty.setText(this.g.getDifficulty());
+		this.showLevel.setText(String.valueOf(this.g.getLevel()));
+		this.showScore.setText(String.valueOf(this.g.getScore()));
 		this.field = new JButton[r][c];
 		for(int i=0;i<r;i++) {
 			for(int j=0;j<c;j++) {
@@ -87,6 +87,12 @@ public class GUI {
 				this.panelField.add(this.field[i][j]);
 			}
 		}
+		this.buttonChangeDifficulty.addMouseListener(new ClickHandler());
+		this.buttonEndGame.addMouseListener(new ClickHandler());
+		this.buttonNewGame.addMouseListener(new ClickHandler());
+		this.buttonChangeDifficulty.setFocusable(false);
+		this.buttonEndGame.setFocusable(false);
+		this.buttonNewGame.setFocusable(false);
 		this.window.addKeyListener(new EventHandler());
 		this.window.setSize(900,700);
 		this.window.setResizable(false);
@@ -95,12 +101,23 @@ public class GUI {
 	}
 	
 	public static void main(String[] args) {
-		GUI G1 = new GUI(10,10);
-		while(!G1.g.isEndGame()) {
+		GUI G1 = new GUI(15,15);
+		while(true) {
 			G1.g.checkGame();
 			G1.renderBoard();
+			if(G1.g.isEndGame()) {
+				if(!G1.g.getWin()) {
+					G1.showDifficulty.setText(G1.g.getDifficulty());
+					G1.showLevel.setText(String.valueOf(G1.g.getLevel()));
+					G1.showScore.setText(String.valueOf(G1.g.getScore()));
+					G1.g.setGame(15,15);
+				}
+				else {
+					G1.showLevel.setText(G1.g.updateLevel());
+					G1.g.newGame(15, 15, G1.g.getLevel(), G1.g.getDifficulty(), G1.g.getScore());
+				}
+			}
 		}
-		G1.window.setVisible(false);
 	}
 	
 	public void renderBoard() {
@@ -134,6 +151,92 @@ public class GUI {
 					this.field[i][j].setEnabled(onf);
 				}
 			}
+		}
+	}
+	
+	public class EventHandler implements KeyListener {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			//GUI.this.enableFocus(GUI.this.g.p.getPosition(), false);
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_NUMPAD1:
+				GUI.this.g.p.moveDownLeft();
+				break;
+			case KeyEvent.VK_NUMPAD2:
+				GUI.this.g.p.moveDown();
+				break;
+			case KeyEvent.VK_NUMPAD3:
+				GUI.this.g.p.moveDownRight();
+				break;
+			case KeyEvent.VK_NUMPAD4:
+				GUI.this.g.p.moveLeft();
+				break;
+			case KeyEvent.VK_NUMPAD6:
+				GUI.this.g.p.moveRight();
+				break;
+			case KeyEvent.VK_NUMPAD7:
+				GUI.this.g.p.moveUpLeft();
+				break;
+			case KeyEvent.VK_NUMPAD8:
+				GUI.this.g.p.moveUp();
+				break;
+			case KeyEvent.VK_NUMPAD9:
+				GUI.this.g.p.moveUpRight();
+				break;
+			}
+			//GUI.this.enableFocus(GUI.this.g.p.getPosition(), true);
+			if(GUI.this.g.getDifficulty().equals("Easy")) {
+				GUI.this.g.list_Pirates.forEach((g)->g.moveToPlayer(GUI.this.g.p.getPosition(),1));
+			}
+			else {
+				GUI.this.g.list_Pirates.forEach((g)->g.moveToPlayer(GUI.this.g.p.getPosition()));
+			}
+		}
+		@Override
+		public void keyReleased(KeyEvent e) {	
+		}
+		@Override
+		public void keyTyped(KeyEvent e) {
+		}
+	}
+	
+	public class ClickHandler implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if(e.getSource().equals(GUI.this.buttonChangeDifficulty)) {
+				if(GUI.this.g.getDifficulty().equals("Easy")) {
+					GUI.this.g.setEasyDifficulty(false);
+				}
+				else {
+					GUI.this.g.setEasyDifficulty(true);
+				}
+				GUI.this.showDifficulty.setText(GUI.this.g.getDifficulty());
+			}
+			else if(e.getSource().equals(GUI.this.buttonNewGame)) {
+				GUI.this.g.newGame(GUI.this.g.b.getRows(), GUI.this.g.b.getColumns(), 1, "Easy", 0);
+			}
+			else if(e.getSource().equals(GUI.this.buttonEndGame)) {
+				System.exit(0);
+				GUI.this.window.setVisible(false);
+				GUI.this.window.dispose();
+			}
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {	
 		}
 	}
 }
--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 
 public class Game {
-	Board b;
-	Player p;
-	ArrayList<Pirate> list_Pirates;
-	ArrayList<Barrier> list_Barriers;
-	int level;
+	public Board b;
+	public Player p;
+	public ArrayList<Pirate> list_Pirates;
+	public ArrayList<Barrier> list_Barriers;
+	private int level;
+	private int score;
+	private String difficulty;
+	private boolean isWin;
 	
 	Game() {
 		this.b = null;
@@ -15,24 +18,45 @@ public class Game {
 		list_Barriers = null;
 	}
 	
-	//Not real function
-	public void setLevel(int i) {
-		this.level = i;
+	public void newGame(int r,int c,int level,String diff,int score) {
+		this.b = new Board(r,c);
+		this.p = new Player(new Position(3,3,b));
+		this.level = level;
+		this.difficulty = diff;
+		this.score = score;
+		this.isWin = true;
+		list_Pirates = new ArrayList<Pirate>();
+		list_Barriers = new ArrayList<Barrier>();
+		while(list_Pirates.size()!=4+this.level) {
+			Position t = new Position((int)(Math.random()*this.b.getRows()),(int)(Math.random()*this.b.getColumns()),this.b);
+			if(!t.isEqual(this.p.getPosition())) {
+				list_Pirates.add(new Pirate(t));
+			}
+		}
+		while(list_Barriers.size()!=3+this.level) {
+			Position t = new Position((int)(Math.random()*this.b.getRows()),(int)(Math.random()*this.b.getColumns()),this.b);
+			if(!t.isEqual(this.p.getPosition())) {
+				list_Barriers.add(new Barrier(t));
+			}
+		}
 	}
 
 	public void setGame(int r,int c) {
 		this.b = new Board(r,c);
 		this.p = new Player(new Position(3,3,b));
 		this.level = 1;
+		this.difficulty = "Easy";
+		this.score = 0;
+		this.isWin = true;
 		list_Pirates = new ArrayList<Pirate>();
 		list_Barriers = new ArrayList<Barrier>();
-		while(list_Pirates.size()!=4*this.level) {
+		while(list_Pirates.size()!=3+this.level) {
 			Position t = new Position((int)(Math.random()*this.b.getRows()),(int)(Math.random()*this.b.getColumns()),this.b);
 			if(!t.isEqual(this.p.getPosition())) {
 				list_Pirates.add(new Pirate(t));
 			}
 		}
-		while(list_Barriers.size()!=3*this.level) {
+		while(list_Barriers.size()!=3+this.level) {
 			Position t = new Position((int)(Math.random()*this.b.getRows()),(int)(Math.random()*this.b.getColumns()),this.b);
 			if(!t.isEqual(this.p.getPosition())) {
 				list_Barriers.add(new Barrier(t));
@@ -46,6 +70,10 @@ public class Game {
 	}
 	
 	private void checkPirates() {
+		Pirate[] arr = new Pirate[this.list_Pirates.size()];
+		for(int i=0;i<this.list_Pirates.size();i++) {
+			arr[i]=this.list_Pirates.get(i);
+		}
 		for(int i=0;i<list_Pirates.size();i++) {
 			for(int j=0;j<list_Pirates.size();j++) {
 				if(i==j) {
@@ -55,16 +83,18 @@ public class Game {
 					this.b.removePirate(list_Pirates.get(i).getPosition());
 					this.b.removePirate(list_Pirates.get(j).getPosition());
 					list_Barriers.add(new Barrier(list_Pirates.get(i).getPosition()));
-					list_Pirates.remove(i);
-					if(j!=0) {
-						list_Pirates.remove(j-1);
-					}
-					else {
-						list_Pirates.remove(j);
-					}
+					arr[i]=null;
+					arr[j]=null;
 				}
 			}
 		}
+		ArrayList<Pirate> temp = new ArrayList<Pirate>();
+		for(Pirate p : arr) {
+			if(p!=null) {
+				temp.add(p);
+			}
+		}
+		this.list_Pirates = temp;
 	}
 	
 	private void checkBarriers() {
@@ -86,14 +116,53 @@ public class Game {
 		}
 		for(int i=0;i<list_Pirates.size();i++) {
 			if(this.list_Pirates.get(i).getPosition().isEqual(this.p.getPosition())) {
+				this.isWin = false;
 				return true;
 			}
 		}
 		for(int i=0;i<list_Barriers.size();i++) {
 			if(this.list_Barriers.get(i).getPosition().isEqual(this.p.getPosition())) {
+				this.isWin = false;
 				return true;
 			}
 		}
 		return false;
 	}
+	
+	public int getLevel() {
+		return this.level;
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+	
+	public void setScore(int i) {
+		this.score+=i;
+	}
+	
+	public String updateLevel() {
+		return String.valueOf(++this.level);
+	}
+
+	public String getDifficulty() {
+		return this.difficulty;
+	}
+	
+	public void setEasyDifficulty(boolean b) {
+		if(b) {
+			this.difficulty="Easy";
+			return;
+		}
+		this.difficulty="Hard";
+	}
+	
+	public boolean getWin() {
+		return this.isWin;
+	}
+	
+	public void setWin(boolean b) {
+		this.isWin = b;
+	}
+	
 }
